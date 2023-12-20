d5d29074d48e7ae88feb041a5ef9350724f649a3
prasan3006 <137260246+prasan3006@users.noreply.github.com>
2023-12-20 12:14:46 +0530

    Add files via upload

---
+++ static/google-analytics.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-R8CH61ZG9C");
---
+++ static/zoho.js
@@ -0,0 +1,15 @@
+var $zoho = $zoho || {};
+$zoho.salesiq = $zoho.salesiq || {
+  widgetcode:
+    "039a1f08c774cad13a42541c820dbfbc8699fffcb99271d9fc167d14105bb3d1e8fafaca392877341c25016baa66ff79",
+  values: {},
+  ready: function () {},
+};
+var d = document;
+s = d.createElement("script");
+s.type = "text/javascript";
+s.id = "zsiqscript";
+s.defer = true;
+s.src = "https://salesiq.zoho.in/widget";
+t = d.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(s, t);
