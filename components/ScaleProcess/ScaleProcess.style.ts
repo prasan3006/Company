8e66916a11375a54bdadd2b3917c2a8e62a71096
prasan3006 <137260246+prasan3006@users.noreply.github.com>
2023-12-20 12:20:37 +0530

    Delete components directory

--- components/ScaleProcess/ScaleProcess.style.ts
+++
@@ -1,5 +0,0 @@
-import { css } from "emotion";
-
-const ScaleProcessStyle = () => css``;
-
-export default ScaleProcessStyle;
--- components/SectionTitle/SectionTitle.style.ts
+++
@@ -1,9 +0,0 @@
-import { css } from "emotion";
-
-const SectionTitleStyle = () => css`
-  .justify-content {
-    text-align: justify;
-  }
-`;
-
-export default SectionTitleStyle;
